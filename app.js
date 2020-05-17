5f52aa38ff1b4c47c140675bec01d8420e22d867
K.Kimura <dotnsf@gmail.com>
2020-05-17 17:44:19 +0900

    interim fix for #8. getColorIndex() not working in doodle.ejs

--- app.js
+++ app.js
@@ -11,6 +11,9 @@ var express = require( 'express' ),
     uuidv1 = require( 'uuid/v1' ),
     app = express();
 
+//. #8
+var QRCode = require( 'qrcode' );
+
 var settings = require( './settings' );
 
 var db = null;
@@ -95,6 +98,46 @@ app.get( '/doodle/:id', function( req, res ){
     });
   }else{
     res.render( 'doodle', { id: id, doodle: doodle } );
+  }
+});
+
+//. #8
+app.post( '/qrcode', function( req, res ){
+  res.contentType( 'application/json; charset=utf-8' );
+
+  if( db ){
+    var data = JSON.parse( '[' + req.body.data + ']' );
+
+    if( data ){
+      var filepath = './tmp/' + uuidv1() + '.png';
+      //. https://www.npmjs.com/package/qrcode
+      QRCode.toFile( 
+        filepath,
+        [ { data: data, mode: 'byte' } ],
+        function( err, result ){
+          if( err ){
+            console.log( err );
+            fs.unlink( filepath, function( err ){} );
+          }else{
+            var bin = fs.readFileSync( filepath );
+            fs.unlink( filepath, function( err ){} );
+            res.contentType( 'image/png' );
+            res.header( { 'Content-Disposition': 'inline' } );
+            res.end( bin, 'binary' );
+          }
+        }
+      );
+    }else{
+      var p = JSON.stringify( { status: false, error: 'no image data.' }, null, 2 );
+      res.status( 400 );
+      res.write( p );
+      res.end();
+    }
+  }else{
+    var p = JSON.stringify( { status: false, error: 'db not ready.' }, null, 2 );
+    res.status( 400 );
+    res.write( p );
+    res.end();
   }
 });
 
